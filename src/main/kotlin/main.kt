f2317a01f922b93d80414fbf64cb340baae05b4f
Stonos <stonos@gmail.com>
2019-08-21 13:39:42 +0300

    Show small donations side by side

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -14,6 +14,8 @@ import org.w3c.dom.HTMLTextAreaElement
 import kotlin.browser.document
 
 const val ITEMS_PER_ROW = 4
+const val ITEMS_PER_ROW_SMALL = 4
+const val SMALL_DONATIONS_PER_ROW = 2
 const val GOLD_ICON = "https://i.imgur.com/mV00LQE.png"
 const val ONE_GOLD = 10000L
 
@@ -163,28 +165,46 @@ private fun TR.printPriceCell(totalPrice: Long) {
 
 fun printDonations(donators: List<Donator>) {
     val output = document.getElementById("output") as HTMLDivElement
+    val bigDonators = donators.filter { it.isBigDonator }
+    val smallDonators = donators.filter { !it.isBigDonator }.chunked(SMALL_DONATIONS_PER_ROW)
     output.append.div {
-        donators.forEach {
+        bigDonators.forEach {
             renderDonator(it)
+        }
+
+        table {
+            classes = setOf("smallDonationsTable")
+            smallDonators.forEach { row ->
+                tr {
+                    row.forEach {
+                        td {
+                            div {
+                                renderDonator(it)
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
 
-private fun DIV.renderDonator(donator: Donator) = div("donator") {
+private fun DIV.renderDonator(donator: Donator) = div("donator" + if (!donator.isBigDonator) " smallDonator" else "") {
+    val itemsPerRow = if (donator.isBigDonator) ITEMS_PER_ROW else ITEMS_PER_ROW_SMALL
     span("donator") { +donator.name }
-    val itemRows = donator.donations.chunked(ITEMS_PER_ROW)
+    val itemRows = donator.donations.chunked(itemsPerRow)
 
     table("donationsTable") {
         itemRows.forEach { row ->
-            if (row.size % 2 == ITEMS_PER_ROW % 2) {
-                renderItems(row)
+            if (row.size % 2 == itemsPerRow % 2) {
+                renderItems(row, donator.isBigDonator, itemsPerRow)
             }
         }
     }
 
     val lastRow = itemRows.last()
-    if (lastRow.size % 2 != ITEMS_PER_ROW % 2) {
-        table("donationsTable") { renderItems(lastRow) }
+    if (lastRow.size % 2 != itemsPerRow % 2) {
+        table("donationsTable") { renderItems(lastRow, donator.isBigDonator, itemsPerRow) }
     }
 
     val totalValue = donator.donations.fold(0L) { accumulator, donation -> accumulator + donation.totalPrice }
@@ -201,26 +221,27 @@ private fun DIV.renderDonator(donator: Donator) = div("donator") {
     }
 }
 
-private fun TABLE.renderItems(items: List<Donation>) = tr {
-    val fakeTdCount = (ITEMS_PER_ROW - items.size) / 2
+private fun TABLE.renderItems(items: List<Donation>, isBigDonator: Boolean, itemsPerRow: Int) = tr {
+    val fakeTdCount = (itemsPerRow - items.size) / 2
     for (i in 0 until fakeTdCount) {
         td {}
     }
-    items.forEach { renderItem(it) }
+    items.forEach { renderItem(it, isBigDonator) }
     for (i in 0 until fakeTdCount) {
         td {}
     }
 }
 
-private fun TR.renderItem(donation: Donation) = td {
+private fun TR.renderItem(donation: Donation, bigDonator: Boolean) = td {
+    val imageSize = if (bigDonator) "64px" else "32px"
     div("itemContainer") {
         //    div(classes = "itemContainer" + if (center) " centerContent" else "") {
         img {
             alt = donation.item.name
             src = donation.item.icon
             classes = setOf("itemIcon")
-            width = "64px"
-            height = "64px"
+            width = imageSize
+            height = imageSize
         }
         span("multiplicationSign") {
             +"×"
--- src/main/kotlin/models/Donator.kt
+++ src/main/kotlin/models/Donator.kt
@@ -1,8 +1,12 @@
 package models
 
+import ONE_GOLD
+
 data class Donator(val name: String, val donations: List<Donation>) : Comparable<Donator> {
     val totalDonation: Long
         get() = donations.fold(0L) { acc, donation -> acc + donation.totalPrice }
+
+    val isBigDonator: Boolean by lazy { totalDonation >= 1000 * ONE_GOLD }
 
     override fun compareTo(other: Donator): Int {
         return totalDonation.compareTo(other.totalDonation)
