e8ef74edbd4bd0a67c249f834a9019161d22eef4
Stonos <stonos@gmail.com>
2020-03-29 18:23:04 +0300

    Matcherino contribution generation script: - Fixed an issue where large donations would get parsed incorrectly - Include "MOTA enthusiasts" as well

--- generate_matcherino_contributions.js
+++ generate_matcherino_contributions.js
@@ -8,12 +8,12 @@ for (let i = 0; i < donations.length; i++) {
     const name = donations[i].getElementsByClassName('user-link-name')[0].innerText;
 
     const amounts = donations[i].getElementsByClassName('amount');
-    const contribution = parseFloat(amounts[0].innerText.substring(1));
+    const contribution = parseFloat(amounts[0].innerText.substring(1).replace(",", ""));
     const type = amounts[1].innerText;
 
     const comment = donations[i].getElementsByClassName('flex-3')[0].innerText;
 
-    if (type === "Direct Contribution" && contribution >= 1.0 && name !== "MightyTeapot") {
+    if ((type === "Direct Contribution" || type === "Masters Of The Arena Enthusiast") && contribution >= 1.0 && name !== "MightyTeapot") {
         output += name + "\t" + contribution + "\t" + type + "\t" + comment + "\n";
     }
 }
