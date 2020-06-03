580b4a130f48ba2d429a1fc0105e68cd499c4b40
Stonos <stonos@gmail.com>
2020-06-04 01:51:26 +0300

    Renamed wis3m0nkey and Philip in the Matcherino donations

--- src/main/kotlin/models/MatcherinoDonor.kt
+++ src/main/kotlin/models/MatcherinoDonor.kt
@@ -1,3 +1,18 @@
 package models
 
-data class MatcherinoDonor(val name: String, val avatar: String)
+data class MatcherinoDonor(val name: String, val avatar: String) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class.js != other::class.js) return false
+
+        other as MatcherinoDonor
+
+        if (name != other.name) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return name.hashCode()
+    }
+}
